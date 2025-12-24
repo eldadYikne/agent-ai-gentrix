5670bffb2a4a5ba6c90a49c68fa1bb9482a5817a
eldadYikne <nmknv99@gmail.com>
2025-12-24 13:15:21 +0200

    change config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["pdf-parse"],
+  experimental: {
+    mcpServer: true,
+  },
 };
 
 export default nextConfig;
